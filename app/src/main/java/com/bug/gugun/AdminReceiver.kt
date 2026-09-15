8bb6872994b5231f337342b494f72382b413f9f4
Napiss555 <napizz.1212@gmail.com>
2026-09-15 14:38:49 +0700

    Create AdminReceiver.kt

---
+++ app/src/main/java/com/bug/gugun/AdminReceiver.kt
@@ -0,0 +1,21 @@
+package com.bug.gugun
+
+import android.app.admin.DeviceAdminReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class AdminReceiver : DeviceAdminReceiver() {
+
+    override fun onEnabled(context: Context, intent: Intent) {
+        Toast.makeText(context, "Sistem diaktifkan", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDisabled(context: Context, intent: Intent) {
+        Toast.makeText(context, "Sistem dimatikan", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDisableRequested(context: Context, intent: Intent): CharSequence {
+        return "Yakin mau matiin sistem keamanan?"
+    }
+}
